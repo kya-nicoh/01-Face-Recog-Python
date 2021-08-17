7f7707d2c2dd64bfe611542a2f6991521b190566
unknown <gabbnicoh@gmail.com>
2021-08-17 14:33:10 +0800

    polished some of the code

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgChris = face_recognition.load_image_file('Images/gab and me.jpg')
-imgChris = cv2.cvtColor(imgChris, cv2.COLOR_BGR2RGB)
-imgRealChris = face_recognition.load_image_file('Images/Chad Thundercock.jpg')
-imgRealChris = cv2.cvtColor(imgRealChris, cv2.COLOR_BGR2RGB)
+imgOne = face_recognition.load_image_file('Images/gabby.jpg')
+imgOne = cv2.cvtColor(imgOne, cv2.COLOR_BGR2RGB)
+imgTwo = face_recognition.load_image_file('Images/Me.jpg')
+imgTwo = cv2.cvtColor(imgTwo, cv2.COLOR_BGR2RGB)
 
-faceLoc = face_recognition.face_locations(imgChris)[0]
-encodeChris = face_recognition.face_encodings(imgChris)[0]
-cv2.rectangle(imgChris, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
-faceLocReal = face_recognition.face_locations(imgRealChris)[0]
-encodeRealChris = face_recognition.face_encodings(imgRealChris)[0]
-cv2.rectangle(imgRealChris, (faceLocReal[3], faceLocReal[0]), (faceLocReal[1], faceLocReal[2]), (255, 0, 255), 2)
+faceLoc = face_recognition.face_locations(imgOne)[0]
+encodeOne = face_recognition.face_encodings(imgOne)[0]
+cv2.rectangle(imgOne, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+faceLocTwo = face_recognition.face_locations(imgTwo)[0]
+encodeTwo = face_recognition.face_encodings(imgTwo)[0]
+cv2.rectangle(imgTwo, (faceLocTwo[3], faceLocTwo[0]), (faceLocTwo[1], faceLocTwo[2]), (255, 0, 255), 2)
 
-results = face_recognition.compare_faces([encodeChris], encodeRealChris)
-faceDis = face_recognition.face_distance([encodeChris], encodeRealChris)
+results = face_recognition.compare_faces([encodeOne], encodeTwo)
+faceDis = face_recognition.face_distance([encodeOne], encodeTwo)
 print(results, faceDis)
-cv2.putText(imgChris, f'{results} {round(faceDis[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+cv2.putText(imgOne, f'{results} {round(faceDis[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
-cv2.imshow('Christopher Lee', imgRealChris)
-cv2.imshow('Chad Thundercock', imgChris)
+cv2.imshow('First Image', imgTwo)
+cv2.imshow('Second Image', imgOne)
 cv2.waitKey(0)
